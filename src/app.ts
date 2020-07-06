304ee140f0a772827ed6b056daf728baa1931d21
Yerzhan Utelbayev <yerzhan.utelbayev@gmail.com>
2020-07-06 22:10:03 +0600

    Add Express application class

---
+++ src/app.ts
@@ -0,0 +1,39 @@
+import path from 'path'
+
+import express from 'express'
+import bodyParser from 'body-parser'
+
+class App {
+  private readonly app: express.Application;
+  private readonly port: string | number;
+  private controllers: any[];
+
+  constructor (controllers: any[], port: string | number) {
+    this.app = express()
+    this.port = port
+    this.controllers = controllers
+  }
+
+  private mountMiddleware (): void {
+    this.app.use(bodyParser.json({ limit: '50mb' }))
+    this.app.use(bodyParser.urlencoded({ limit: '50mb', extended: true }))
+    this.app.use(
+      '/uploads',
+      express.static(path.join(__dirname, '../public/uploads'))
+    )
+  }
+
+  private mountControllers (): void {
+    this.controllers.forEach((controller) => {
+      this.app.use('/', controller.router)
+    })
+  }
+
+  public listen (): void {
+    this.app.listen(this.port, () => {
+      console.log(`Server started at port ${this.port}`)
+    })
+  }
+}
+
+export default App
