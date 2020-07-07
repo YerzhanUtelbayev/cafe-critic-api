c2a0a938f202d4fbb9d828cdbee5696e2aefd619
Yerzhan Utelbayev <yerzhan.utelbayev@gmail.com>
2020-07-07 12:52:38 +0600

    Add Facility interface

---
+++ src/interfaces/facilty.interface.ts
@@ -0,0 +1,13 @@
+export default interface Facility {
+  _id: string;
+  owner: string;
+  title: string;
+  description: string;
+  promoImage: string;
+  ratings?: {
+    overaLL: number,
+    food: number,
+    service: number,
+    interior: number,
+  }
+}
