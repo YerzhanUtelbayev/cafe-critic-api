ef9eda78bfe671b38083a671f3fbc6a6e3459464
Yerzhan Utelbayev <yerzhan.utelbayev@gmail.com>
2020-07-08 14:11:16 +0600

    Add controllers and middlewares initialization to App

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,12 @@
 import path from 'path'
 
 import express from 'express'
+import mongoose from 'mongoose'
 import bodyParser from 'body-parser'
+import cookieParser from 'cookie-parser'
 
 import Controller from './interfaces/controller.interface'
+import errorMiddleware from './middleware/error.middleware'
 
 class App {
   private readonly app: express.Application;
@@ -14,18 +17,28 @@ class App {
     this.app = express()
     this.port = port
     this.controllers = controllers
+
+    this.connectToTheDatabase()
+    this.initializeMiddleware()
+    this.initializeControllers()
+    this.initializeErrorHandling()
   }
 
-  private mountMiddleware (): void {
+  private initializeMiddleware (): void {
     this.app.use(bodyParser.json({ limit: '50mb' }))
     this.app.use(bodyParser.urlencoded({ limit: '50mb', extended: true }))
+    this.app.use(cookieParser())
     this.app.use(
       '/uploads',
       express.static(path.join(__dirname, '../public/uploads'))
     )
   }
 
-  private mountControllers (): void {
+  private initializeErrorHandling ():void {
+    this.app.use(errorMiddleware)
+  }
+
+  private initializeControllers (): void {
     this.controllers.forEach((controller) => {
       this.app.use('/', controller.router)
     })
@@ -35,6 +48,19 @@ class App {
     this.app.listen(this.port, () => {
       console.log(`Server started at port ${this.port}`)
     })
+  }
+
+  public getServer (): express.Application {
+    return this.app
+  }
+
+  private connectToTheDatabase (): void {
+    const {
+      MONGO_USER,
+      MONGO_PASSWORD,
+      MONGO_PATH
+    } = process.env
+    mongoose.connect(`mongodb://${MONGO_USER}:${MONGO_PASSWORD}${MONGO_PATH}`)
   }
 }
 
