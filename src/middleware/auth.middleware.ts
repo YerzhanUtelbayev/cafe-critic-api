d664b7098dca74a6b7d0e9b22c463aa7f08377e0
Yerzhan Utelbayev <yerzhan.utelbayev@gmail.com>
2020-07-08 13:11:41 +0600

    Add authentication middleware

---
+++ src/middleware/auth.middleware.ts
@@ -0,0 +1,42 @@
+import { Response, NextFunction } from 'express'
+import jwt from 'jsonwebtoken'
+
+import AuthenticationTokenMissingException from '../exceptions/AuthenticationTokenMissingException'
+import WrongAuthenticationTokenException from '../exceptions/WrongAuthenticationTokenException'
+import DataStoredInToken from '../interfaces/dataStoredInToken.interface'
+import RequestWithUser from '../interfaces/requestWithUser.interface'
+import userModel from '../models/user.model'
+
+async function authMiddleware (
+  request: RequestWithUser,
+  response: Response,
+  next: NextFunction
+): Promise<void> {
+  const { cookies } = request.cookies
+  if (cookies && cookies.Authorization && process.env.JWT_SECRET) {
+    // TODO: add exception for undefined process.env.JWT_SECRET
+    const secret = process.env.JWT_SECRET
+    const token = cookies.Authorization
+    try {
+      const verificationResponse = (await jwt.verify(
+        token,
+        secret
+      )) as DataStoredInToken
+      const id = verificationResponse._id
+      const user = await userModel.findById(id)
+
+      if (user) {
+        request.user = user
+        return next()
+      } else {
+        return next(new WrongAuthenticationTokenException())
+      }
+    } catch (error) {
+      return next(new WrongAuthenticationTokenException())
+    }
+  } else {
+    return next(new AuthenticationTokenMissingException())
+  }
+}
+
+export default authMiddleware
