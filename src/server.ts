4f3f99676c8d67326bb19efe8ea101495485fc15
Yerzhan Utelbayev <yerzhan.utelbayev@gmail.com>
2020-07-08 19:11:29 +0600

    Add App initialization

--- src/server.ts
+++ src/server.ts
@@ -1,4 +1,11 @@
 import 'dotenv/config'
 import validateEnv from './utilities/validateEnv'
 
+import App from './app'
+import FacilityController from './controllers/facility/facility.controller'
+
 validateEnv()
+
+const app = new App([new FacilityController()], process.env.port || 8000)
+
+app.listen()
