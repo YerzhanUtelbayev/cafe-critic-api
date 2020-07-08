e9ba4e12b2542be54928d17fd8ff8e9d2133675f
Yerzhan Utelbayev <yerzhan.utelbayev@gmail.com>
2020-07-08 16:25:22 +0600

    Add validation middleware

---
+++ src/middleware/validation.middleware.ts
@@ -0,0 +1,29 @@
+import { plainToClass } from 'class-transformer'
+import { validate, ValidationError } from 'class-validator'
+import { RequestHandler } from 'express'
+
+import HttpException from '../exceptions/HttpExceptions'
+
+function validationMiddleware<T> (
+  type: never,
+  skipMissingProperties = false
+): RequestHandler {
+  return (request, response, next) => {
+    validate(plainToClass(type, request.body), { skipMissingProperties }).then(
+      (errors: ValidationError[]) => {
+        if (errors.length > 0) {
+          const messages = errors.filter(
+            ({ constraints }) =>
+              constraints && Object.keys(constraints).length > 0
+          )
+          const message = messages.join(', ')
+          next(new HttpException(400, message))
+        } else {
+          next()
+        }
+      }
+    )
+  }
+}
+
+export default validationMiddleware
