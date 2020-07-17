451409232bbf2c03a7da7245add848a1193b41cd
Yerzhan Utelbayev <yerzhan.utelbayev@gmail.com>
2020-07-17 15:51:57 +0600

    Fix missing 4th parameter in error middleware

--- src/middleware/error.middleware.ts
+++ src/middleware/error.middleware.ts
@@ -1,14 +1,16 @@
-import { Request, Response } from 'express'
+import { Request, Response, NextFunction } from 'express'
 
 import HttpExceptions from '../exceptions/HttpExceptions'
 
 function errorMiddleware (
   error: HttpExceptions,
   request: Request,
-  response: Response
+  response: Response,
+  _next: NextFunction
 ):Response {
   const status = error.status || 500
   const message = error.message || 'Something went wrong'
+  console.log(error)
   return response.status(status).send({ message, status })
 }
 
