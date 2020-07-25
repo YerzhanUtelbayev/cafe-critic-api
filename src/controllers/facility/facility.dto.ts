4c062fe1fae19e8e4ee7fe9a0e93facd7d756f7f
Yerzhan Utelbayev <yerzhan.utelbayev@gmail.com>
2020-07-25 12:53:36 +0600

    Fix missimg property in Facility DTO

--- src/controllers/facility/facility.dto.ts
+++ src/controllers/facility/facility.dto.ts
@@ -16,11 +16,15 @@ class CreateFacilityDto implements Facility {
   @IsString()
   public promoImage: string;
 
+  @IsString()
+  public thumbnail: string;
+
   constructor (facility: Facility) {
     this.owner = facility.owner
     this.title = facility.title
     this.description = facility.description
     this.promoImage = facility.promoImage
+    this.thumbnail = facility.thumbnail
   }
 }
 
